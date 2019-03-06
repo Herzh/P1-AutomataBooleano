a8f6ff0aa9b20fb6d72ee214ab86a6fc9f494f85
herzh <alucin_nm@hotmail.com>
2019-03-05 21:34:41 -0600

    caracteres validos funcionando

---
+++ src/p1/automatabooleano/P1AutomataBooleano.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package p1.automatabooleano;
+
+import GUI.Main;
+
+/**
+ *
+ * @author Jedidian
+ */
+public class P1AutomataBooleano {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Main inicio = new Main();
+        inicio.setVisible(true);
+    }
+    
+}
